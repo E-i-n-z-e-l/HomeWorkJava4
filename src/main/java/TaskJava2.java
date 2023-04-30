de4592e0754edc40b4c55933d0c32e27ab536343
Einzel <einzel0102@mail.ru>
2023-04-30 14:59:59 +0800

    "Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список."

---
+++ src/main/java/TaskJava2.java
@@ -0,0 +1,23 @@
+/*
+Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+ */
+import java.util.LinkedList;
+
+public class TaskJava2 {
+    public static void main(String[] args) {
+        LinkedList<String> listLink = new LinkedList<>();
+        listLink.add("apple");
+        listLink.add("banana");
+        listLink.add("orange");
+        listLink.add("pineapple");
+        listLink.add("lemon");
+        for (String element : listLink) {
+            System.out.println(element);
+        }
+        System.out.println("__________________________________________________________________");
+        for (int i = listLink.size() - 1; i >= 0; i--) {
+            String element = listLink.get(i);
+            System.out.println(element);
+        }
+    }
+}
